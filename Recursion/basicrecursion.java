6d4e9a6905bc9a44f52d79d704aa71685f4a5dc7
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-12-04 12:10:18 +0530

    Add files via upload

---
+++ Recursion/basicrecursion.java
@@ -0,0 +1,21 @@
+
+class basicrecursion {
+
+ static int c=0;
+ static void print() {
+
+    if(c==3) return ;
+    System.out.println(c);
+
+    c++;
+    print();
+ }
+
+
+ public static void main(String args []){
+
+        print();
+
+    } 
+}
+
